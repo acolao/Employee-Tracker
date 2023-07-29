7042e2c49d931c7818d7b8969013083c590e7ef0
Alison Colao <alison.siwek@gmail.com>
2023-07-29 14:16:59 -0700

    added functions

--- index.js
+++ index.js
@@ -1,42 +1,25 @@
-const { prompt } = require("inquirer");
-const db = require("./server");
-
-Infinity();
+const { Prompt } = require('inquirer');
+const fs = require('fs');
+const db = require('./db/connection');
 
 function loadMainPrompts() {
   prompt([
     {
       type: "list",
       name: "choice",
-      message: "What would you like to do?",
-      choices: [
+      message: "Select an option",
+       choices: [
         {
           name: "View All Employees",
           value: "VIEW_EMPLOYEES"
-        },
-        {
-          name: "View All Employees By Department",
-          value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
-        },
-        {
-          name: "View All Employees By Manager",
-          value: "VIEW_EMPLOYEES_BY_MANAGER"
         },
         {
           name: "Add Employee",
           value: "ADD_EMPLOYEE"
         },
         {
-          name: "Remove Employee",
-          value: "REMOVE_EMPLOYEE"
-        },
-        {
           name: "Update Employee Role",
           value: "UPDATE_EMPLOYEE_ROLE"
-        },
-        {
-          name: "Update Employee Manager",
-          value: "UPDATE_EMPLOYEE_MANAGER"
         },
         {
           name: "View All Roles",
@@ -47,10 +30,6 @@ function loadMainPrompts() {
           value: "ADD_ROLE"
         },
         {
-          name: "Remove Role",
-          value: "REMOVE_ROLE"
-        },
-        {
           name: "View All Departments",
           value: "VIEW_DEPARTMENTS"
         },
@@ -59,41 +38,26 @@ function loadMainPrompts() {
           value: "ADD_DEPARTMENT"
         },
         {
-          name: "Remove Department",
-          value: "REMOVE_DEPARTMENT"
-        },
-        {
-          name: "View Total Utilized Budget By Department",
-          value: "VIEW_UTILIZED_BUDGET_BY_DEPARTMENT"
-        },
-        {
           name: "Quit",
           value: "QUIT"
         }
       ]
   
-    }
-]).then(res => {
+    } 
+
+    ])
+    .then(res => {
     let choice = res.choice;
 
     switch (choice) {
         case "VIEW_EMPLOYEES":
             viewEmployees();
             break;
-        case "VIEW_EMPLOYEES_BY_DEPARTMENT":
-            viewEmployeesByDepartment();
-            break;
-        case "VIEW_EMPLOYEES_BY_MANAGER":
-            viewEmployeesByManager();
-            break;
         case "ADD_EMPLOYEE":
             addEmployee();
             break;
-        case "REMOVE_EMPLOYEE":
-            removeEmployee();
-            break;
-        case "UPDATE_EMPLOYEE_MANAGER":
-            updateEmployeeManager();
+        case "UPDATE_EMPLOYEE_ROLE":
+            updateEmployeeRole();
             break;
         case "VIEW_ROLES":
             viewRoles();
@@ -101,35 +65,26 @@ function loadMainPrompts() {
         case "ADD_ROLE":
             addRole();
             break;
-        case "REMOVE_ROLE":
-            removeRole();
-            break;
         case "VIEW_DEPARTMENTS":
             viewDepartments();
             break;
         case "ADD_DEPARTMENT":
             addDepartment();
             break;
-        case "REMOVE_DEPARTMENT":
-            removeDepartment();
-            break;
-        case "VIEW_UTILIZED_BUDGET_BY_DEPARTMENT":
-            viewUtilizedBudgetByDepartment();
-            break;
         default:
             quit();
     }
 
-function viewEmployees(){
-    db.findAllEmployees()
-    .then(([rows])=> {
-        let employees=rows;
-        console.log("\n");
-        console.table(employees);
-    })
-    .then(()=>loadMainPrompts());
+});
+
 }
 
-
-
-});
+function viewDepartments() {
+    db.findAllDepartments()
+    .then(([rows]) => {
+        let departments = rows;
+        console.log("\n");
+        console.table(departments);
+    })
+    .then(() => loadMainPrompts());
+}
